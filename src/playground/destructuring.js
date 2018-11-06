37cc39c5a358726ff6f1a7f0dc0961b247e540b1
Michael Gubik <gubikmic@gmail.com>
2018-11-06 18:01:50 +0800

    Initial commit

---
+++ src/playground/destructuring.js
@@ -0,0 +1,43 @@
+// Object Destructuring
+
+// import { isNumber } from "util";
+
+// const person = {
+//   name: 'Michael',
+//   age: 32,
+//   location: {
+//     city: 'Canggu',
+//     temp: 32
+//   }
+// }
+
+// const {name: firstName = 'Anonymous', age} = person
+// console.log(`${firstName} is ${age}`)
+
+// const {city, temp: temperature } = person.location
+// if (city && isNumber(temperature)) {
+//   console.log(`It's ${temperature} in ${city}`)
+// }
+
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// }
+
+// const { name: publisherName = 'Self-Published' } = book.publisher
+// console.log(publisherName)
+
+//
+// Object Destructuring
+//
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+const [, city, state] = address
+console.log(`You are in ${city} ${state}.`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75']
+const [beverage, , mediumPrice] = item
+console.log(`A medium ${beverage} costs ${mediumPrice}`)
